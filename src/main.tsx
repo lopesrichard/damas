f0f0039718023a162a2708733655919600fb7ed8
Richard Lopes <contato.richardlopes@outlook.com>
2022-03-15 00:56:23 -0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Damas } from './components/damas'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Damas />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
