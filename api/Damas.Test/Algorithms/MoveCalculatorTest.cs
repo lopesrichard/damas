04dabe5ddf5cf18df932535c4de6aadc9aadf619
Richard Lopes <contato.richardlopes@outlook.com>
2022-03-17 17:23:38 -0300

    feat: implement dama non capture moves

--- api/Damas.Test/Algorithms/MoveCalculatorTest.cs
+++ api/Damas.Test/Algorithms/MoveCalculatorTest.cs
@@ -24,7 +24,7 @@ public class MoveCalculatorTest
     }
 
     // ==============================================================================================
-    // ====================================== CAPTURE TESTS =========================================
+    // =============================== REGULAR PIECE CAPTURE TESTS ==================================
     // ==============================================================================================
 
     // -------------------------------------
@@ -153,7 +153,7 @@ public class MoveCalculatorTest
     }
 
     // ==============================================================================================
-    // ==================================== NON CAPTURE TESTS =======================================
+    // ============================= REGULAR PIECE NON CAPTURE TESTS ================================
     // ==============================================================================================
 
     // -------------------------------------
@@ -209,5 +209,104 @@ public class MoveCalculatorTest
         Assert.AreEqual(1, thirdTree.CountNodes());
         Assert.AreEqual(new Position(5, 1), thirdTree.Root.Value);
         Assert.That(thirdTree.Root.Children, Is.Empty);
+    }
+
+    // ==============================================================================================
+    // ================================ DAMA PIECE CAPTURE TESTS ====================================
+    // ==============================================================================================
+
+    // ==============================================================================================
+    // ============================== DAMA PIECE NON CAPTURE TESTS ==================================
+    // ==============================================================================================
+
+    // -------------------------------------
+    // The following scenario is returned
+    // -------------------------------------
+    // | 7 |   |   |   |   |   |   |   |   |
+    // | 6 |   |   |   |   |   |   |   |   |
+    // | 5 |   | W |   |   |   |   |   |   |
+    // | 4 |   |   |   |   | B |   | B |   |
+    // | 3 |   |   |   |   |   | B |   |   |
+    // | 2 | W |   |   |   | W |   |   |   |
+    // | 1 |   | B |   |   |   |   |   |   |
+    // | 0 |   |   | B |   |   |   |   |   |
+    // | - | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
+    // -------------------------------------
+    private Match GetDamaNonCaptureTestCase()
+    {
+        return BuildMatch(new List<Piece>()
+        {
+            new Piece(Guid.NewGuid(), new Position(1, 5), Color.WHITE, true, false),
+            new Piece(Guid.NewGuid(), new Position(0, 2), Color.WHITE, true, false),
+            new Piece(Guid.NewGuid(), new Position(4, 2), Color.WHITE, true, false),
+            new Piece(Guid.NewGuid(), new Position(1, 1), Color.BLACK, false, false),
+            new Piece(Guid.NewGuid(), new Position(2, 0), Color.BLACK, false, false),
+            new Piece(Guid.NewGuid(), new Position(4, 4), Color.BLACK, false, false),
+            new Piece(Guid.NewGuid(), new Position(5, 3), Color.BLACK, false, false),
+            new Piece(Guid.NewGuid(), new Position(6, 4), Color.BLACK, false, false),
+        });
+    }
+
+    [Test]
+    public void ShouldReturnCorrectTreeOfDamaNonCaptureMoves()
+    {
+        var match = GetDamaNonCaptureTestCase();
+
+        var calculator = new MoveCalculator();
+
+        var moves = calculator.Calculate(match);
+
+        Assert.That(moves, Has.Count.EqualTo(3));
+
+        var firstTree = moves.First().Value;
+        Assert.AreEqual(7, firstTree.CountNodes());
+        Assert.AreEqual(new Position(1, 5), firstTree.Root.Value);
+        Assert.That(firstTree.Root.Children, Has.Count.EqualTo(6));
+
+        Assert.AreEqual(new Position(0, 6), firstTree.Root.Children[0].Value);
+        Assert.AreEqual(new Position(2, 6), firstTree.Root.Children[1].Value);
+        Assert.AreEqual(new Position(3, 7), firstTree.Root.Children[2].Value);
+        Assert.AreEqual(new Position(0, 4), firstTree.Root.Children[3].Value);
+        Assert.AreEqual(new Position(2, 4), firstTree.Root.Children[4].Value);
+        Assert.AreEqual(new Position(3, 3), firstTree.Root.Children[5].Value);
+
+        Assert.That(firstTree.Root.Children[0].Children, Is.Empty);
+        Assert.That(firstTree.Root.Children[1].Children, Is.Empty);
+        Assert.That(firstTree.Root.Children[2].Children, Is.Empty);
+        Assert.That(firstTree.Root.Children[3].Children, Is.Empty);
+        Assert.That(firstTree.Root.Children[4].Children, Is.Empty);
+        Assert.That(firstTree.Root.Children[5].Children, Is.Empty);
+
+        var secondTree = moves.Skip(1).First().Value;
+        Assert.AreEqual(new Position(0, 2), secondTree.Root.Value);
+        Assert.That(secondTree.Root.Children, Has.Count.EqualTo(5));
+
+        Assert.AreEqual(new Position(1, 3), secondTree.Root.Children[0].Value);
+        Assert.AreEqual(new Position(2, 4), secondTree.Root.Children[1].Value);
+        Assert.AreEqual(new Position(3, 5), secondTree.Root.Children[2].Value);
+        Assert.AreEqual(new Position(4, 6), secondTree.Root.Children[3].Value);
+        Assert.AreEqual(new Position(5, 7), secondTree.Root.Children[4].Value);
+
+        Assert.That(secondTree.Root.Children[0].Children, Is.Empty);
+        Assert.That(secondTree.Root.Children[1].Children, Is.Empty);
+        Assert.That(secondTree.Root.Children[2].Children, Is.Empty);
+        Assert.That(secondTree.Root.Children[3].Children, Is.Empty);
+        Assert.That(secondTree.Root.Children[4].Children, Is.Empty);
+
+        var thirdTree = moves.Skip(2).First().Value;
+        Assert.AreEqual(new Position(4, 2), thirdTree.Root.Value);
+        Assert.That(secondTree.Root.Children, Has.Count.EqualTo(5));
+
+        Assert.AreEqual(new Position(3, 3), thirdTree.Root.Children[0].Value);
+        Assert.AreEqual(new Position(2, 4), thirdTree.Root.Children[1].Value);
+        Assert.AreEqual(new Position(3, 1), thirdTree.Root.Children[2].Value);
+        Assert.AreEqual(new Position(5, 1), thirdTree.Root.Children[3].Value);
+        Assert.AreEqual(new Position(6, 0), thirdTree.Root.Children[4].Value);
+
+        Assert.That(thirdTree.Root.Children[0].Children, Is.Empty);
+        Assert.That(thirdTree.Root.Children[1].Children, Is.Empty);
+        Assert.That(thirdTree.Root.Children[2].Children, Is.Empty);
+        Assert.That(thirdTree.Root.Children[3].Children, Is.Empty);
+        Assert.That(thirdTree.Root.Children[4].Children, Is.Empty);
     }
 }
--- api/Damas/Algorithms/DamaPieceMoveCalculator.cs
+++ api/Damas/Algorithms/DamaPieceMoveCalculator.cs
@@ -8,7 +8,48 @@ namespace Damas.Algorithms
     {
         public GeneralTree<Position> Calculate(Match match, Piece piece)
         {
-            throw new NotImplementedException();
+            var tree = new GeneralTree<Position>(piece.Position);
+
+            CalculateCaptureMoves(match, piece, tree.Root);
+
+            if (tree.Root.Children.Count > 0)
+            {
+                return tree;
+            }
+
+            CalculateNonCaptureMoves(match, tree.Root);
+
+            return tree;
+        }
+
+        private void CalculateCaptureMoves(Match match, Piece piece, GeneralTreeNode<Position> node)
+        {
+        }
+
+        private void CalculateNonCaptureMoves(Match match, GeneralTreeNode<Position> node)
+        {
+            var position = node.Value;
+
+            var append = (Func<Position, Position> move) =>
+            {
+                var condition = (Position position) =>
+                {
+                    return match.IsValidPosition(position) && !match.IsPositionOccupied(position);
+                };
+
+                for (var current = move(position); condition(current); current = move(current))
+                {
+                    if (match.IsPositionAvaialable(current))
+                    {
+                        node.Append(current);
+                    }
+                }
+            };
+
+            append(position => position.Northwest());
+            append(position => position.Northeast());
+            append(position => position.Southwest());
+            append(position => position.Southeast());
         }
     }
 }
