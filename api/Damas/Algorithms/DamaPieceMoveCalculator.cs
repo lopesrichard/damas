433fd7cc25ee2dd9b943d3dd0fb4e68e754d96c5
Richard Lopes <contato.richardlopes@outlook.com>
2022-03-17 22:43:35 -0300

    feat: implement dama piece capture moves

--- api/Damas/Algorithms/DamaPieceMoveCalculator.cs
+++ api/Damas/Algorithms/DamaPieceMoveCalculator.cs
@@ -1,5 +1,6 @@
 using Damas.DataStructures;
 using Damas.Entities;
+using Damas.Exceptions;
 using Damas.Structs;
 
 namespace Damas.Algorithms
@@ -24,32 +25,103 @@ namespace Damas.Algorithms
 
         private void CalculateCaptureMoves(Match match, Piece piece, GeneralTreeNode<Position> node)
         {
+            var directions = GetDirections(node.Value, (position) =>
+           {
+               return match.IsValidPosition(position);
+           });
+
+            foreach (var direction in directions)
+            {
+                var previous = node.Value;
+                var append = false;
+
+                foreach (var position in direction)
+                {
+                    var current = match.GetPieceAt(position);
+
+                    if (current == null)
+                    {
+                        if (append)
+                        {
+                            var child = node.Append(position);
+
+                            var backup = match.GetPieceAt(previous);
+
+                            if (backup == null)
+                            {
+                                throw new NullPieceException();
+                            }
+
+                            piece.Position = position;
+                            match.CapturePiece(backup);
+
+                            CalculateCaptureMoves(match, piece, child);
+
+                            piece.Position = node.Value;
+                            match.RestorePiece(backup);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        if (current.IsCaptured)
+                        {
+                            continue;
+                        }
+
+                        if (append)
+                        {
+                            break;
+                        }
+
+                        if (current.Color == piece.Color)
+                        {
+                            break;
+                        }
+
+                        append = true;
+                        previous = position;
+                    }
+                }
+            }
         }
 
         private void CalculateNonCaptureMoves(Match match, GeneralTreeNode<Position> node)
         {
-            var position = node.Value;
-
-            var append = (Func<Position, Position> move) =>
+            var directions = GetDirections(node.Value, (position) =>
             {
-                var condition = (Position position) =>
-                {
-                    return match.IsValidPosition(position) && !match.IsPositionOccupied(position);
-                };
+                return match.IsValidPosition(position) && !match.IsPositionOccupied(position);
+            });
 
-                for (var current = move(position); condition(current); current = move(current))
+            foreach (var direction in directions)
+            {
+                foreach (var position in direction)
                 {
-                    if (match.IsPositionAvaialable(current))
+                    if (match.IsPositionAvaialable(position))
                     {
-                        node.Append(current);
+                        node.Append(position);
                     }
                 }
-            };
+            }
+        }
 
-            append(position => position.Northwest());
-            append(position => position.Northeast());
-            append(position => position.Southwest());
-            append(position => position.Southeast());
+        private IEnumerable<IEnumerable<Position>> GetDirections(Position initial, Func<Position, bool> condition)
+        {
+            yield return GetPositions(initial, condition, position => position.Northwest());
+            yield return GetPositions(initial, condition, position => position.Northeast());
+            yield return GetPositions(initial, condition, position => position.Southwest());
+            yield return GetPositions(initial, condition, position => position.Southeast());
+        }
+
+        private IEnumerable<Position> GetPositions(Position initial, Func<Position, bool> condition, Func<Position, Position> move)
+        {
+            for (var current = move(initial); condition(current); current = move(current))
+            {
+                yield return current;
+            }
         }
     }
 }
