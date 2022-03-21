76a55dcb07523319a7d16f78d95f3a3af7c1ddec
Richard Lopes <contato.richardlopes@outlook.com>
2022-03-21 11:32:23 -0300

    feat: configure database converters

--- api/Damas.Data/ModelBuilderExtension.cs
+++ api/Damas.Data/ModelBuilderExtension.cs
@@ -1,5 +1,7 @@
 using System.Reflection;
+using Damas.Core.Enums;
 using Damas.Core.Structs;
+using Damas.Data.Converters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
@@ -8,28 +10,34 @@ namespace Damas.Data
 {
     public static class ModelBuilderExtension
     {
+        public static IDictionary<Type, ValueConverter> Converters = new Dictionary<Type, ValueConverter>()
+        {
+            { typeof(Position), new PositionConverter() },
+            { typeof(Color), new EnumToStringConverter<Color>() },
+            { typeof(MessageType), new EnumToStringConverter<MessageType>() },
+        };
+
         public static void Configure(this ModelBuilder builder)
         {
             foreach (var entity in builder.Model.GetEntityTypes())
             {
-                var properties = entity.ClrType.GetProperties();
-
-                builder.UseValueConverter<Position>(new PositionConverter(), entity, properties);
+                foreach (var property in entity.ClrType.GetProperties())
+                {
+                    if (Converters.TryGetValue(property.PropertyType, out var converter))
+                    {
+                        builder.UseValueConverter(converter, entity, property);
+                    }
+                }
             }
         }
 
-        private static ModelBuilder UseValueConverter<T>(
+        private static void UseValueConverter(
             this ModelBuilder builder,
             ValueConverter converter,
             IMutableEntityType entity,
-            PropertyInfo[] properties)
+            PropertyInfo property)
         {
-            foreach (var property in properties.Where(p => p.PropertyType == typeof(T)))
-            {
-                builder.Entity(entity.Name).Property(property.Name).HasConversion(converter);
-            }
-
-            return builder;
+            builder.Entity(entity.Name).Property(property.Name).HasConversion(converter);
         }
     }
 }
